2a28acc3fd5b5efdd4308d7598036b75cd7e7b29
japygo <hyunnjinn@gmail.com>
2024-04-02 22:22:20 +0900

    @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -16,11 +17,17 @@ import tobyspring.config.MyAutoConfiguration;
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory(Environment env) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+//        factory.setContextPath(env.getProperty("contextPath"));
+        System.out.println("contextPath = " + contextPath);
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
