c38511fb48ec2c6cfbdaadce33b69dccf630dead
japygo <hyunnjinn@gmail.com>
2024-04-03 18:06:52 +0900

    리포지토리를 사용하는 HelloService

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -1,13 +1,25 @@
 package tobyspring.helloboot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HelloBootApplication {
+    private final JdbcTemplate jdbcTemplate;
+
+    public HelloBootApplication(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostConstruct
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
 
     @Bean
     ApplicationRunner applicationRunner(Environment env) {
